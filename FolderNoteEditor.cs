7e956b8b85121071734f546dc288e4f4e4f0ed6f
hannes delbeke <hannesdelbeke@gmail.com>
2025-03-25 05:14:45 -0700

    add all asset support
    certain exceptions wont yet work

--- FolderNoteEditor.cs
+++ FolderNoteEditor.cs
@@ -1,44 +1,47 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
 
 [InitializeOnLoad]
-public class FolderNoteEditor : Editor
+public class AssetNoteEditor : Editor
 {
-    static string previousSelectedFolderPath = "";
-    static string selectedFolderPath = "";
+    static string previousSelectedAssetPath = "";
+    static string selectedAssetPath = "";
     //public static string folderNote = "";
 
-    static FolderNoteEditor()
+    static AssetNoteEditor()
     {
         //Selection.selectionChanged += OnSelectionChanged;
     }
 
     private static void OnSelectionChanged()
     {
+        // print asset type, great when we need to add note support for new asset types
+        // Debug.Log("Selection changed to " + Selection.activeObject);
         string selectedObjectPath = AssetDatabase.GetAssetPath(Selection.activeObject);
-
-        if (AssetDatabase.IsValidFolder(selectedObjectPath) && selectedObjectPath != selectedFolderPath)
+        if (AssetDatabase.AssetPathExists(selectedObjectPath) && selectedObjectPath != selectedAssetPath)
         {
-            previousSelectedFolderPath = selectedFolderPath;
-            selectedFolderPath = selectedObjectPath;
+            previousSelectedAssetPath = selectedAssetPath;
+            selectedAssetPath = selectedObjectPath;
         }
         else
         {
-            selectedFolderPath = "";
+            selectedAssetPath = "";
         }
     }
 
-    [CustomEditor(typeof(DefaultAsset))]
-    public class FolderInspector : Editor
+    [CustomEditor(typeof(UnityEngine.Object), true)]
+    public class AssetInspector : Editor
     {
-
-        private static string folderNote = "";
+        private static string assetNote = "";
         private string lastSavedNote = "";
         private bool isEditing = false;
+        private Vector2 scrollPosition = Vector2.zero; // Keeps track of the scroll position
 
         private void OnEnable()
-        // runs when we select a folder
         {
+            // runs when we select a asset
             // before we get new variables, save the old note
             SaveSelectedNote();
 
@@ -48,9 +51,11 @@ public class FolderNoteEditor : Editor
 
         public override void OnInspectorGUI()
         {
-            if (AssetDatabase.IsValidFolder(AssetDatabase.GetAssetPath(target)))
+            if (AssetDatabase.AssetPathExists(AssetDatabase.GetAssetPath(target)))
             {
-                OnFolderInspectorGUI();
+                base.OnInspectorGUI();
+                var isFolder = target.GetType() == typeof(DefaultAsset);
+                OnFolderInspectorGUI(isFolder);
             }
             else
             {
@@ -58,52 +63,90 @@ public class FolderNoteEditor : Editor
             }
         }
 
-        private void OnFolderInspectorGUI()
+        private void OnFolderInspectorGUI(bool isFolder)
         {
-            EditorGUI.EndDisabledGroup();  // HACK Workaround for unity disabling UI in the inspector for folders
+            if (isFolder)
+            {
+                EditorGUI.EndDisabledGroup();  // HACK Workaround for unity disabling UI in the inspector for folders
+            }
 
-            EditorGUILayout.LabelField("Folder Notes", EditorStyles.boldLabel);
+            EditorGUILayout.LabelField("Notes", EditorStyles.boldLabel);
 
             // Detect start and end of editing
             EditorGUI.BeginChangeCheck();
-            GUI.SetNextControlName("FolderNoteTextArea");
-            folderNote = EditorGUILayout.TextArea(folderNote, GUILayout.Height(60));
+            GUI.SetNextControlName("NoteTextArea");
+
+            scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, GUILayout.Height(100)); // Set the desired height
+            assetNote = EditorGUILayout.TextArea(assetNote, GUILayout.ExpandHeight(true)); // Expand to fit the scrollable view
+            EditorGUILayout.EndScrollView(); // End the scrollable area
 
             isEditing = EditorGUI.EndChangeCheck();  // Track that editing is in progress
 
-            if (isEditing && Event.current.type == EventType.Repaint && GUI.GetNameOfFocusedControl() != "FolderNoteTextArea")
+            if (isEditing && Event.current.type == EventType.Repaint && GUI.GetNameOfFocusedControl() != "NoteTextArea")
             {
                 // Save the note only when focus is lost
                 isEditing = false;
-                if (folderNote != lastSavedNote)
+                if (assetNote != lastSavedNote)
                 {
                     SaveSelectedNote();
                 }
             }
 
-            EditorGUI.BeginDisabledGroup(true);
+            if (isFolder)
+            {
+                EditorGUI.BeginDisabledGroup(true);
+            }
         }
 
         public void LoadNote()
         {
-            if (!string.IsNullOrEmpty(selectedFolderPath))
+            if (!string.IsNullOrEmpty(selectedAssetPath))
             {
-                AssetImporter importer = AssetImporter.GetAtPath(selectedFolderPath);
-                folderNote = importer.userData;
-                lastSavedNote = folderNote;
+                AssetImporter importer = AssetImporter.GetAtPath(selectedAssetPath);
+                assetNote = importer.userData;
+                lastSavedNote = assetNote;
             }
         }
 
         private void SaveSelectedNote()
-        // save the current note to the folder's .meta file
-        // assumes we keep the same folder object selected, e.g. when losing focus on the text area
         {
-            if (!string.IsNullOrEmpty(selectedFolderPath))
+            if (!string.IsNullOrEmpty(selectedAssetPath))
             {
-                AssetImporter importer = AssetImporter.GetAtPath(selectedFolderPath);
-                importer.userData = folderNote;
-                lastSavedNote = folderNote;
+                // save the current note to the folder's .meta file
+                // assumes we keep the same asset selected, e.g. when losing focus on the text area
+                AssetImporter importer = AssetImporter.GetAtPath(selectedAssetPath);
+                importer.userData = assetNote;
+                lastSavedNote = assetNote;
             }
         }
     }
+
+    // there are many exceptions to the custom inspector
+    // it might be easier to create our own editor window
+
+    [CustomEditor(typeof(MonoScript))]
+    public class ScriptInspector : AssetInspector
+    {
+    }
+
+    [CustomEditor(typeof(TextAsset))]
+    public class TextInspector : AssetInspector
+    {
+    }
+
+    [CustomEditor(typeof(Material))]
+    public class MaterialInspector : AssetInspector
+    {
+    }
+
+    //[CustomEditor(typeof(UniversalRendererData))]
+    //public class UniversalRendererDataInspector : AssetInspector
+    //{
+    //}
+
+    //[CustomEditor(typeof(UniversalRenderPipelineAsset))]
+    //public class UniversalRenderPipelineAssetInspector : AssetInspector
+    //{
+    //    // breaks UI formatting in the inspector, likely uses an internal custom inspector
+    //}
 }
